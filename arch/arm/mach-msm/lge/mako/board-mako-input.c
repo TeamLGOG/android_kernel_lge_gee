446043a931c6d6ea4e194361420ac0562174a51c
houst0nn <houstonn@gmail.com>
2013-01-27 07:22:36 -0500

    Update arch/arm/mach-msm/lge/mako/board-mako-input.c
    touch-keys capacitive keys-

--- arch/arm/mach-msm/lge/mako/board-mako-input.c
+++ arch/arm/mach-msm/lge/mako/board-mako-input.c
@@ -27,13 +27,14 @@
 #include <mach/board.h>
 
 /* keypad */
+#include <linux/regulator/gpio-regulator.h>
 #include <linux/mfd/pm8xxx/pm8921.h>
 
 /* i2c */
 #include <linux/regulator/consumer.h>
 #include <linux/i2c.h>
-
 #include <linux/earlysuspend.h>
+#include <linux/input/touch_synaptics_rmi4_i2c.h>
 #include <linux/input/lge_touch_core.h>
 #include <mach/board_lge.h>
 #include "board-mako.h"
@@ -111,13 +112,15 @@ static struct touch_power_module touch_pwr = {
 };
 
 static struct touch_device_caps touch_caps = {
-	.button_support = 0,
-	.is_width_major_supported = 1,
-	.is_width_minor_supported = 0,
+	.button_support = 1,
+	.y_button_boundary = 0,
+	.number_of_button = 3,
+	.button_name = {KEY_BACK,KEY_HOMEPAGE,KEY_MENU},
+	.button_margin	= 0,
+	.is_width_supported = 1,
 	.is_pressure_supported = 1,
 	.is_id_supported = 1,
-	.max_width_major = 15,
-	.max_width_minor = 15,
+	.max_width = 15,
 	.max_pressure = 0xFF,
 	.max_id = 10,
 	.lcd_x = 768,
@@ -128,17 +131,18 @@ static struct touch_device_caps touch_caps = {
 
 static struct touch_operation_role touch_role = {
 	.operation_mode = INTERRUPT_MODE,
-	.key_type = KEY_NONE,
+	.key_type = TOUCH_HARD_KEY,
 	.report_mode = REDUCED_REPORT_MODE,
 	.delta_pos_threshold = 1,
 	.orientation = 0,
+	.report_period	= 10000000,
 	.booting_delay = 400,
-	.reset_delay = 20,
+	.reset_delay = 5,
 	.suspend_pwr = POWER_OFF,
 	.resume_pwr = POWER_ON,
 	.jitter_filter_enable = 0,
 	.jitter_curr_ratio = 30,
-	.accuracy_filter_enable = 0,
+	.accuracy_filter_enable = 1,
 	.irqflags = IRQF_TRIGGER_FALLING,
 	.show_touches = 0,
 	.pointer_location = 0,
@@ -161,10 +165,127 @@ static struct i2c_board_info synaptics_ts_info[] = {
 	},
 };
 
+
+
+
+
+       rc = gpio_request(TOUCH_RESET, "rmi4_reset_pin");
+       if (rc) {
+       	pr_err("%s: Failed to get rmi4_reset_pin %d. Code: %d.",
+       						__func__, TOUCH_RESET, rc);
+       	return rc;
+       }            
+       rc = gpio_direction_output(TOUCH_RESET, 1);
+
+
+
+	//Touch IC Power On
+	vreg_l15 = regulator_get(NULL, "8921_l15");   //3.3V_TOUCH_VDD, VREG_L15: 2.75 ~ 3.3
+	if (IS_ERR(vreg_l15)) {
+		pr_err("%s: regulator get of 8921_l15 failed (%ld)\n", __func__, PTR_ERR(vreg_l15));
+		rc = PTR_ERR(vreg_l15);
+		return rc;
+	}
+	
+	pr_err("%s: [Touch D] S2 \n",__func__);
+	vreg_l22 = regulator_get(NULL, "8921_l22");   //1.8V_TOUCH_IO, VREG_L22: 1.7 ~ 2.85
+	if (IS_ERR(vreg_l22)) {
+		pr_err("%s: regulator get of 8921_l22 failed (%ld)\n", __func__, PTR_ERR(vreg_l22));
+		rc = PTR_ERR(vreg_l22);
+		return rc;
+	}
+	pr_err("%s: [Touch D] S3 \n",__func__);
+	rc = regulator_set_voltage(vreg_l15, 3300000, 3300000);
+	rc = regulator_set_voltage(vreg_l22, 1800000, 1800000);
+
+
+	regulator_enable(vreg_l15);
+	regulator_enable(vreg_l22);
+
+
+	if (configure) {
+		rc = gpio_request(data->gpio_number, "rmi4_attn");
+		if (rc) {
+			pr_err("%s: Failed to get attn gpio %d. Code: %d.",
+								__func__, data->gpio_number, rc);
+			return rc;
+		}
+
+		gpio_tlmm_config(GPIO_CFG(data->gpio_number, 0, GPIO_CFG_INPUT,
+								GPIO_CFG_PULL_UP, GPIO_CFG_6MA),GPIO_CFG_ENABLE);
+
+		rc = gpio_direction_input(data->gpio_number);
+		if (rc) {
+				pr_err("%s: Failed to setup attn gpio %d. Code: %d.",
+				__func__, data->gpio_number, rc);
+				gpio_free(data->gpio_number);
+		}
+	} else {
+			pr_warn("%s: No way to deconfigure gpio %d.",
+			__func__, data->gpio_number);
+	}
+
+	if(rc < 0){
+			printk(KERN_INFO "[Touch D] %s: cannot request GPIO\n", __func__);
+			return rc;
+	}
+	
+	printk("[Touch D]synaptics_touchpad_gpio_setup -- \n");
+
+	return rc;
+}
+
+#define AXIS_ALIGNMENT { \
+	.swap_axes = false, \
+	.flip_x = false, \
+	.flip_y = false, \
+	.clip_X_low  = 0, \
+	.clip_Y_low  = 0, \
+	.offset_X = 0,  \
+	.offset_Y = 0, \
+}
+
+#define TM2144_ADDR 0x20
+#define TM2144_ATTN 6
+
+static unsigned char tm2144_button_codes[] = {KEY_BACK,KEY_HOMEPAGE,KEY_MENU}; 
+static struct rmi_f1a_button_map tm2144_button_map = {
+	.nbuttons = ARRAY_SIZE(tm2144_button_codes),
+	.map = tm2144_button_codes,
+};
+
+static struct syna_gpio_data tm2144_gpiodata = {
+	.gpio_number = TM2144_ATTN,
+	.gpio_name = "ts_int.gpio_6",
+};
+
+static struct rmi_device_platform_data tm2144_platformdata = {
+	.driver_name = "rmi_generic",
+	.sensor_name = "TM2144",
+	.attn_gpio = TM2144_ATTN,
+	.attn_polarity = RMI_ATTN_ACTIVE_LOW,
+	.gpio_data = &tm2144_gpiodata,
+	.gpio_config = synaptics_touchpad_gpio_setup,
+	.axis_align = AXIS_ALIGNMENT,
+	.f1a_button_map = &tm2144_button_map,
+	.reset_delay_ms = 100,
+};
+
+
+static struct i2c_board_info synaptics_ds4_rmi_info[] = {
+     [0] = {
+         I2C_BOARD_INFO("rmi_i2c", TM2144_ADDR),
+        .platform_data = &tm2144_platformdata,
+     },
+};
+
+
 void __init apq8064_init_input(void)
 {
-	printk(KERN_INFO "[Touch D] %s: NOT DCM KDDI, reg synaptics driver \n",
-	       __func__);
+
+	printk(KERN_INFO "[Touch D] %s: NOT DCM KDDI, reg synaptics driver \n", __func__);
 	i2c_register_board_info(APQ8064_GSBI3_QUP_I2C_BUS_ID,
-				&synaptics_ts_info[0], 1);
+		&synaptics_ts_info[0], 1);
+
 }
+
